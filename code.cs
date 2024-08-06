cbd2c3cc09f7267cdc925979a17cda8021f2ebbf
AstikKishev <kishev160696@gmail.com>
2024-08-06 18:03:47 +0300

    cycle

--- code.cs
+++ code.cs
@@ -7,3 +7,15 @@
 
 array1[] myFantasy = new string[5] {"27", "564", "big", "black", "coc", "hi"};
 array2[] myFantasy1 = new string[myFantasy.Length];
+void susy (array1[] myFantasy, array2[] myFantasy1)
+{
+    int count = 0;
+    for (int i = 0; i < myFantasy.Length; i++)
+    {
+    if(myFantasy[i].Length <= 3)
+        {
+        myFantasy1[count] = myFantasy[i];
+        count++;
+        }
+    }
+}
